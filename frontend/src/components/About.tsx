776fc222b764f183a805a5e403aed3ee55ada4f6
Shaurav-Bhandari <shauravbhandaari2@gmail.com>
2025-04-20 23:15:22 +0545

    Third commit. The hero Section and 50% of About Section is finished!!!

---
+++ frontend/src/components/About.tsx
@@ -0,0 +1,52 @@
+import { useGSAP} from "@gsap/react";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function About() {
+    useGSAP(() => {
+        const clipAnimation =  gsap.timeline({
+            scrollTrigger:{
+                trigger: '#clip',
+                start: 'center center',
+                end: '+=800 center',
+                scrub: 0.2,
+                pin: true,
+                pinSpacing: true,
+            }
+        });
+        clipAnimation.to('.mask-clip-path', {
+            width: '100vw',
+            height: '100vh',
+            borderRadius: 0
+        })
+
+    });
+    return (
+        <div id={"About"} className="min-h-screen w-screen">
+            <div className={"relative mb-8 mt-36 flex flex-col items-center gap-5"}>
+                <h2 className={"font-general text-sm uppercase md:text-[10px]"}>
+                    Welcome to Pathivara Innovations!
+                </h2>
+                <div className={"mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]"}>
+                    We are your partners in digital transformation.
+                </div>
+                <div className={"about-subtext "}>
+                    <p>
+                        We craft bespoke software, cloud integrations and strategic consulting to help businesses thrive in a fast‑moving landscape—combining technical expertise, creative flair and an unwavering commitment to excellence.
+                    </p>
+                </div>
+            </div>
+            <div className={"h-dvh w-screen"} id="clip">
+                <div className={"mask-clip-path about-image"}>
+                    <img
+                        src={"img/about.webp"}
+                        alt={"background"}
+                        className={"absolute top-0 left-0 size-full object-cover"}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
